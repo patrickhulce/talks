ee89a5c9e45f2c9831e97968a6657d0cc2b2b7d8
Patrick Hulce <patrick.hulce@gmail.com>
2018-10-25 15:48:05 -0500

    fix: restructure for multiple talks

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-for f in $(find . -type f -name "*.mdx"); do
- mdx-deck build "$f"
+rm -rf ./dist ./dist-www
+
+for f in $(find ./src -type f -name "*.mdx"); do
+  echo "Building $f..."
+  mdx-deck build "$f"
+
+  IFS=/ read -ra PARTS <<< "$f"
+  # Parts is ./src/<year>/<talk>/deck.mdx split on /
+  # We want <year> and <talk> so index 2 and 3
+  OUT_DIR="./dist-www/${PARTS[2]}/${PARTS[3]}"
+  mkdir -p "$OUT_DIR"
+  mv ./dist/* "$OUT_DIR/"
 done
